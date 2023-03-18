ab478dfd042299b37a3aed93a83eb5c5875fa518
priyanshu10 <priyanshuparate@gmail.com>
2023-03-18 21:49:04 +0530

    solved 2011. Final Value of Variable After Performing Operations

---
+++ src/main/java/FinalValueOfVariable.java
@@ -0,0 +1,14 @@
+public class FinalValueOfVariable {
+    public static void main(String[] args) {
+        String[] input = {"--X", "X++", "X++"};
+        System.out.println(finalValueAfterOperations(input));
+    }
+
+    public static int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (String s : operations) {
+            result = s.contains("+") ? ++result : --result;
+        }
+        return result;
+    }
+}
